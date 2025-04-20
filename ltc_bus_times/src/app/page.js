1c220b212a2879f306cd7ba6dcfff6c1c6ddfe6d
RileyWong26 <rileywongwong@gmail.com>
2025-04-19 20:50:01 -0400

    Main page

--- ltc_bus_times/src/app/page.js
+++ ltc_bus_times/src/app/page.js
@@ -1,30 +1,15 @@
 'use client'
 import Image from "next/image";
 import React, {useEffect, useState} from "react";
+import Routes from "./Routes";
 
 export default function Home() {
-  
-  const [routes, setRoutes] = useState([])
-
-  const fetchRoutes = async() => {
-    fetch('http://127.0.0.1:5001/Routes', {
-      method:'GET'
-    })
-    .then(response => response.json())
-    .then(data => setRoutes(data))
-    .catch(error => console.log(error));
-  }
-
  
-  return (
+   return (
     <div className=" items-center content-center">
       <input className=" border-2 border-black" onClick={() => console.log(routes)}/>
-      <button onClick={() => fetchRoutes()}>fetch</button>
-      <button onClick={() => console.log(routes)}>display</button>
 
-      {routes.map((item) => (
-        <p key={item['Route ID']}>{item.Abreviation}</p>
-      ))}
+      <Routes />
     </div>
   );
 }
